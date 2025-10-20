40dd4e1595fea25e74dbdb102070ca378099cb84
blufx44 <blufx44@vt.edu>
2025-10-20 16:05:51 -0400

    Beercss
    Added beercss dependency and hooked it up to always using the dark theme - can customize in index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import "beercss";
 import './index.css';
 import App from './app';
 import { Provider } from 'react-redux';
