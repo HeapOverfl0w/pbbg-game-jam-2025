8d93c256da59cfd7f60548fc622895a73a283084
HeapOverfl0w <jgregorybest@gmail.com>
2025-10-25 15:03:19 -0400

    more styling stuff

--- src/splashscreen/splashscreen.tsx
+++ src/splashscreen/splashscreen.tsx
@@ -21,18 +21,28 @@ export function Splashscreen(props: SplashscreenProps) {
 
   return (
     <div className='absolute top left' style={{ width: '100%', height: '100%' }}>
-      <article className='padding absolute center middle bottom-align center-align' style={{ width: '50%', height: '50%' }} >
-        {status.percent !== '100' &&
-          <div style={{ width: '100%'}}>
-            <progress value={status.percent} style={{ width: '100%'}} max='100'></progress>
-            <h4>{status.status}</h4>
+      <article className='padding absolute center middle' style={{ width: '70%', height: '70%', display: 'flex', flexDirection: 'row' }} >
+        <img src='./img/castle.png' style={{height: '100%'}}/>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', textAlign: 'center', flex: 1, gap: '10%'}}>
+          <h2 style={{color: '#b62a3c', marginTop: '10%'}}>Blood Moon: Of Angels and Demons (Temp Title)</h2>
+          <h4 style={{width: '60%'}}>
+            When the Blood Moon rose and refused to wane, the veil twixt Heaven and Hell was sundered. Through long forgotten portals, angels and demons alike now tread upon mortal soil and their endless war rekindled beneath crimson skies. Amidst the ruin of men, only those who endure the night may glimpse the dawn.
+          </h4>
+          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end', flex: 1}}>
+            {status.percent !== '100' &&
+              <div style={{ width: '100%'}}>
+                <progress value={status.percent} style={{ width: '100%'}} max='100'></progress>
+                <h4>{status.status}</h4>
+              </div>
+            }
+            {status.percent === '100' &&
+              <button onClick={() => props.onStart()}>
+                Start
+              </button>
+            }
           </div>
-        }
-        {status.percent === '100' &&
-          <button onClick={() => props.onStart()}>
-            Start
-          </button>
-        }
+        </div>
+        
       </article>
     </div>
   );
